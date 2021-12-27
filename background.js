1d484d63d2623274e7af7e9031c9131e577f5544
leoBonnaire <laithaut.lisser@gmail.com>
2021-12-27 11:17:14 +0100

    JS on Ctrl key

--- background.js
+++ background.js
@@ -1,12 +1,8 @@
-chrome.commands.onCommand.addListener((command) => {
-  if (command === 'hello') {
-    console.log("Hello there! ^^");
-    chrome.windows.getCurrent(function (currentWindow) {
-      chrome.tabs.query({ active: true, windowId: currentWindow.id }, function (activeTabs) {
-          activeTabs.map(function (tab) {
-              chrome.tabs.executeScript(tab.id, { file: 'insert.js', allFrames: false });
-          });
-      });
-  });
+chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+  if (changeInfo.status == 'complete') {	
+	chrome.scripting.executeScript({
+			target: {tabId: tabId, allFrames: true},
+			files: ['insert.js']
+	});
   }
-});
+})
--- insert.js
+++ insert.js
@@ -1,21 +1,29 @@
-const canvas = document.createElement('canvas');
 
-canvas.id = "my-canvas";
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-canvas.style.top = '0px';
-canvas.style.position = "absolute";
-canvas.style.zIndex = 999;
+// const canvas = document.createElement('canvas');
 
-const body = document.getElementsByTagName("body")[0];
-body.appendChild(canvas);
+// canvas.id = "my-canvas";
+// canvas.width = window.innerWidth;
+// canvas.height = window.innerHeight;
+// canvas.style.top = '0px';
+// canvas.style.position = "absolute";
+// canvas.style.zIndex = 999;
+
+// const body = document.getElementsByTagName("body")[0];
+// body.appendChild(canvas);
 
 
 // Some optional drawings.
-const ctx = canvas.getContext("2d");
-ctx.fillStyle = "rgba(255, 0, 0, 0.2)";
-ctx.fillRect(100, 100, 200, 200);
-ctx.fillStyle = "rgba(0, 255, 0, 0.2)";
-ctx.fillRect(150, 150, 200, 200);
-ctx.fillStyle = "rgba(0, 0, 255, 0.2)";
-ctx.fillRect(200, 50, 200, 200);
+// const ctx = canvas.getContext("2d");
+// ctx.fillStyle = "rgba(255, 0, 0, 0.2)";
+// ctx.fillRect(100, 100, 200, 200);
+// ctx.fillStyle = "rgba(0, 255, 0, 0.2)";
+// ctx.fillRect(150, 150, 200, 200);
+// ctx.fillStyle = "rgba(0, 0, 255, 0.2)";
+// ctx.fillRect(200, 50, 200, 200);
+
+document.addEventListener('keydown', (e) => {
+	if(e.key === "Control") {
+		console.log("Hello World");
+		
+	}
+});
